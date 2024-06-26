ae39cc579a8f4a9302a808b3836b992777a96f3a
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-06-26 15:35:32 -0300

    add keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,68 @@
+import { useState } from "react";
+
 import GridItem from "./components/GridItem";
 import KeyboardButton from "./components/KeyboardButton";
 
+import { keys } from "./mocks/Keyboard";
+
 const App = () => {
+  const [targetWord, setTargetWord] = useState("frizz");
+  const [guess, setGuess] = useState<string[]>([]);
+  const [confirmedGuess, setConfirmedGuess] = useState<string[]>([]);
+
+  // useEffect(() => {
+  //   fetch("https://wordle-api-lemon.vercel.app/word")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setTargetWord(data);
+  //     });
+  // }, []);
+
+  const handleCharSelect = (char: string) => {
+    setGuess([...guess, char]);
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+  };
+
+  const handleConfirm = () => {
+    setConfirmedGuess(guess);
+    setGuess([]);
+  };
+
+  console.log(guess);
+
   return (
     <>
-      <h1>Wordle App</h1>
+      <h1 className="text-theme-light-gray text-2xl">{targetWord}</h1>
       <div className="flex">
-        <GridItem char="a" />
-        <GridItem char="" />
-        <GridItem char="" />
-        <GridItem char="" />
+        {Array.from(targetWord).map((_, index) => (
+          <GridItem char={guess[index]} />
+        ))}
       </div>
-      <KeyboardButton char="a" />
-      <KeyboardButton char="b" />
-      <KeyboardButton char="c" />
+      {keys.map((char) => (
+        <KeyboardButton
+          disabled={guess.length >= targetWord.length}
+          char={char}
+          onClick={() => handleCharSelect(char)}
+        />
+      ))}
+      <button
+        onClick={handleDelete}
+        className="text-theme-light-gray text-2xl border"
+      >
+        delete
+      </button>
+      <button
+        onClick={handleConfirm}
+        className="text-theme-light-gray text-2xl border"
+      >
+        enter
+      </button>
     </>
   );
 };
--- src/components/KeyboardButton.tsx
+++ src/components/KeyboardButton.tsx
@@ -1,10 +1,20 @@
 interface KeyboardButtonProps {
   char?: string;
+  onClick: () => void;
+  disabled?: boolean;
 }
 
-const KeyboardButton: React.FC<KeyboardButtonProps> = ({ char }) => {
+const KeyboardButton: React.FC<KeyboardButtonProps> = ({
+  char,
+  onClick,
+  disabled,
+}) => {
   return (
-    <button className="text-theme-light-gray text-2xl p-3 border">
+    <button
+      className="text-theme-light-gray text-2xl p-3 border"
+      onClick={onClick}
+      disabled={disabled}
+    >
       {char}
     </button>
   );
